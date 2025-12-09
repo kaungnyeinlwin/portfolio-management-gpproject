392feb68a6d3bac8196e8e8f83252bbd1ff77318
kaungnyeinlwin <63581347+kaungnyeinlwin@users.noreply.github.com>
2025-12-09 15:05:02 +0700

    Delete webservice/src/main/java/org/global/academy/UseStock.java

--- webservice/src/main/java/org/global/academy/UseStock.java
+++
@@ -1,8 +0,0 @@
-package org.global.academy;
-
-public class UseStock {
-    public static void main(String[] args) {
-        Stock nvid = new Stock("NVIDIA", "NVDA", "NASDAQ", 170.0);
-        System.out.println(nvid.getSymbol());
-    }
-}
